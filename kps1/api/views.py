bd9d6761f3e6ca3b66f16b98ae3286e042aa4da3
Kamalpreet-3223 <ksingh7_be20@thapar.edu>
2021-07-26 04:12:13 +0530

    Done with Serializers

--- kps1/api/views.py
+++ kps1/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
+
+def stuinfo(request):
+    stu = Student.objects.all()
+    serializer = StudentSerializer(stu, many=True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+def studetail(request, id):
+    stu = Student.objects.get(pk=id)
+    serializer = StudentSerializer(stu)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
